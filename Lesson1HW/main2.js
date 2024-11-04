330d3475d564092f22c1cfc52fa315aed5187441
Uliia17 <45678julia@gmail.com>
2024-11-04 15:03:29 +0200

    [add]:[file]: .gitignore, Lesson1HW

---
+++ Lesson1HW/main2.js
@@ -0,0 +1,5 @@
+let firstName = 'Юлія';
+let middleName = 'Ігорівна';
+let lastName = 'Польська';
+let msg = firstName + ' ' + middleName + ' ' + lastName;
+console.log(msg);
---
+++ Lesson1HW/main4.js
@@ -0,0 +1,6 @@
+let FirstName = prompt("Ім'я","Юлія");
+console.log(FirstName);
+let MiddleName = prompt("По батькові","Ігорівна");
+console.log(MiddleName);
+let Age = prompt("Вік", "33");
+console.log(Age);
