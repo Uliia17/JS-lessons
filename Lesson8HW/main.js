221007181a904049771ab4aa75a69c049e0c5a84
Uliia17 <45678julia@gmail.com>
2024-11-20 18:23:39 +0200

    [add]:[file]: Lesson8.html, main.js

---
+++ Lesson8HW/main.js
@@ -0,0 +1,33 @@
+function cloner(obj) {
+    if (obj === undefined || obj === null || Number.isNaN(obj)) {
+        return obj;
+    }
+    if (obj) {
+        let functions = [];
+        for (const key in obj) {
+            if (typeof obj[key] === 'function') {
+                const functionClone = obj[key].bind({});
+                functions.push({key, functionClone});
+            }
+        }
+        const cloneObject = JSON.parse(JSON.stringify(obj));
+        for (const {key, functionClone} of functions) {
+            cloneObject[key] = functionClone;
+        }
+        console.log(cloneObject);
+        return cloneObject;
+    }
+    return false;
+}
+const clone = cloner({ id: 1, name: "John", age: undefined, address: null, score: NaN, foo() { console.log('client'); } });
+clone.foo();
+//-----------------------------------------------------------------
+let coursesAndDurationArray = [
+    {title: 'JavaScript Complex', monthDuration: 5},
+    {title: 'Java Complex', monthDuration: 6},
+    {title: 'Python Complex', monthDuration: 6},
+    {title: 'QA Complex', monthDuration: 4},
+    {title: 'FullStack', monthDuration: 7},
+    {title: 'Frontend', monthDuration: 4}
+];
+console.log(coursesAndDurationArray.map((course, index) => ({...course, id: index + 1})));
